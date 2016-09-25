5f224e84474ee74ab300b5429fd7ea14280f3dc6
Thiru Paramasivan <thiru@apple.com>
2016-09-26 00:28:19 +0100

    <rdar://problem/27849557> pie spark git changes (#85)
    <rdar://problem/27849557> pie spark git changes - temp commit to handle build failure.

--- rio-maven-custom-build.sh
+++ rio-maven-custom-build.sh
@@ -142,6 +142,8 @@ echo -e "MAVEN_OPTS exported: ${MAVEN_OPTS}"
 execute_command "./dev/change-scala-version.sh" $SCALA_VERSION "$@"
 
 ##Get POM Variables
+$("$MVN" help:evaluate -Dexpression=project.artifactId -X $@)
+
 POM_PROJECT_ARTIFACT_ID=$("$MVN" help:evaluate -Dexpression=project.artifactId $@ 2>/dev/null | grep -v "INFO" | tail -n 1)
 POM_PROJECT_VERSION=$("$MVN" help:evaluate -Dexpression=project.version $@ 2>/dev/null | grep -v "INFO" | tail -n 1)
 POM_SCALA_VERSION=$("$MVN" help:evaluate -Dexpression=scala.binary.version $@ 2>/dev/null\
