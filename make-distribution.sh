a3add63211ddf857f70c8c8398c6a9dad010bce7
zhou-jiang <zhou_jiang@apple.com>
2017-02-10 15:22:47 -0800

    Include Hadoop version in distribution version.

--- make-distribution.sh
+++ make-distribution.sh
@@ -155,8 +155,10 @@ if [[ "$VERSION" == *-SNAPSHOT ]]; then
 	TIMED_SNAPSHOT=`date -u +"%Y%m%d.%H%M%S"`
 	ARTIFACT_VERSION=`echo $VERSION | sed -e 's/-SNAPSHOT$/-'$TIMED_SNAPSHOT'-1/g'`
 	echo "Snapshot mapped to timed artifact version: $ARTIFACT_VERSION"
+	TGZ_VERSION=`echo $VERSION | sed -e 's/-SNAPSHOT$/-'$SPARK_HADOOP_VERSION'-SNAPSHOT/g'`
 else
 	ARTIFACT_VERSION="$VERSION"
+	TGZ_VERSION="${VERSION}"
 fi
 
 SPARK_DISTRIBUTION_FILE_NAME="spark-$ARTIFACT_VERSION-bin-$NAME.tar.gz"
@@ -287,5 +289,5 @@ if [ "$MAKE_TGZ" == "true" ]; then
   LOCAL_REPO_DIR="$SPARK_HOME/.dist/local-repo"
   mkdir -p "${LOCAL_REPO_DIR}"
   LOCAL_REPO="file://${LOCAL_REPO_DIR}"
-  "$MVN" deploy:deploy-file -DgroupId="com.apple.pie.spark" -DartifactId="spark-distribution_${SCALA_VERSION}" -Dversion="${VERSION}" -Dfile="${SPARK_DISTRIBUTION_FILE_NAME}" -Durl="${LOCAL_REPO}" -Dpackaging=tgz
+  "$MVN" deploy:deploy-file -DgroupId="com.apple.pie.spark" -DartifactId="spark-distribution_${SCALA_VERSION}" -Dversion="${TGZ_VERSION}" -Dfile="${SPARK_DISTRIBUTION_FILE_NAME}" -Durl="${LOCAL_REPO}" -Dpackaging=tgz
 fi
