f556a9f76084f7dbba6949eecb2cfc95ce244662
zhou-jiang <zhou_jiang@apple.com>
2017-02-14 14:39:42 -0800

    <rdar://problem/29271982> change to use maven for spark jarvis scheduler artifact file name etc.

--- make-distribution.sh
+++ make-distribution.sh
@@ -282,6 +282,14 @@ if [ "$MAKE_TGZ" == "true" ]; then
   cp -r "$DISTDIR" "$TARDIR"
   tar czf "$SPARK_DISTRIBUTION_FILE_NAME" -C "$SPARK_HOME" "$TARDIR_NAME"
   rm -rf "$TARDIR"
-  mkdir -p "$SPARK_HOME/.dist/local-repo/com/apple/pie/spark/spark-distribution_$SCALA_VERSION/$TGZ_VERSION"
-  mv "$SPARK_DISTRIBUTION_FILE_NAME" "$SPARK_HOME/.dist/local-repo/com/apple/pie/spark/spark-distribution_$SCALA_VERSION/$TGZ_VERSION/$SPARK_DISTRIBUTION_FILE_NAME"
+  LOCAL_REPO_DIR="$SPARK_HOME/.dist/local-repo"
+  mkdir -p "${LOCAL_REPO_DIR}"
+  LOCAL_REPO="file://${LOCAL_REPO_DIR}"
+  "$MVN" deploy:deploy-file -DgroupId="com.apple.pie.spark" -DartifactId="spark-distribution_${SCALA_VERSION}" -Dversion="${TGZ_VERSION}" -Dfile="${SPARK_DISTRIBUTION_FILE_NAME}" -Durl="${LOCAL_REPO}" -Dpackaging=tgz
+fi
+
+
+if [[ "$TGZ_VERSION" == *-SNAPSHOT ]]; then
+	find ./.dist/local-repo/com/apple/pie/spark/ -name "*.tgz" -exec bash -c 'mv $0 $(echo "$0" | sed -e  "s/-[[:digit:]]+\.[[:digit:]]+-[[:digit:]]+\.tgz/-SNAPSHOT.tgz/" )' '{}' \;
+	find ./.dist/local-repo/com/apple/pie/spark/ -name "*.pom" -exec bash -c 'mv $0 $(echo "$0" | sed -e  "s/-[[:digit:]]+\.[[:digit:]]+-[[:digit:]]+\.pom/-SNAPSHOT.pom/" )' '{}' \;
 fi
