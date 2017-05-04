59920ed1be8f513a386f24a2b16c1b65af02ea14
vishwa <vishwa@apple.com>
2017-05-04 12:57:44 -0700

    Add LoggingHandler to the pipeline

--- common/network-common/src/main/java/org/apache/spark/network/TransportContext.java
+++ common/network-common/src/main/java/org/apache/spark/network/TransportContext.java
@@ -22,6 +22,7 @@ import java.util.List;
 import com.google.common.collect.Lists;
 import io.netty.channel.Channel;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.timeout.IdleStateHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -135,6 +136,7 @@ public class TransportContext {
     try {
       TransportChannelHandler channelHandler = createChannelHandler(channel, channelRpcHandler);
       channel.pipeline()
+        .addLast("logger", new LoggingHandler())
         .addLast("encoder", encoder)
         .addLast(TransportFrameDecoder.HANDLER_NAME, NettyUtils.createFrameDecoder())
         .addLast("decoder", decoder)
