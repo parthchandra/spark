ec4a86523c493d0c8f0ff2febf980d6c52a5644e
zhou-jiang <zhou_jiang@apple.com>
2018-02-14 17:34:20 +0000

    Create rio build and apply fixes for insights checkpointing / jarvis mode / ajax on SparkUI. Fix dependency verison

--- streaming/src/main/scala/org/apache/spark/streaming/Checkpoint.scala
+++ streaming/src/main/scala/org/apache/spark/streaming/Checkpoint.scala
@@ -60,7 +60,11 @@ class Checkpoint(ssc: StreamingContext, val checkpointTime: Time)
       "spark.yarn.credentials.file",
       "spark.yarn.credentials.renewalTime",
       "spark.yarn.credentials.updateTime",
-      "spark.ui.filters")
+      "spark.ui.filters",
+      "spark.ui.port",
+      "spark.blockManager.port",
+      "spark.broadcast.port",
+      "spark.fileserver.port")
 
     val newSparkConf = new SparkConf(loadDefaults = false).setAll(sparkConfPairs)
       .remove("spark.driver.host")
