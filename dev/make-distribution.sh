366a97e795f9ed41f8a90f630986ce84458236d8
Shruti Gumma <shruti_gumma@apple.com>
2018-05-21 18:27:44 -0700

    Changed version of slugs

--- dev/make-distribution.sh
+++ dev/make-distribution.sh
@@ -36,6 +36,7 @@ DISTDIR="$SPARK_HOME/dist"
 
 USE_EXISTING_BUILD=false
 
+HADOOP_PROVIDED=false
 MAKE_TGZ=false
 MAKE_PIP=false
 MAKE_R=false
@@ -73,6 +74,10 @@ while (( "$#" )); do
       ;;
     --mvn)
       MVN="$2"
+      shift
+      ;;
+    --hadoop-provided)
+      HADOOP_PROVIDED=true
       shift
       ;;
     --name)
@@ -146,9 +151,17 @@ if [ "$NAME" == "none" ]; then
 fi
 
 if [[ "$VERSION" == *-SNAPSHOT ]]; then
-	TGZ_VERSION=`echo $VERSION | sed -e 's/-SNAPSHOT$/-'$SPARK_HADOOP_VERSION'-SNAPSHOT/g'`
+    if [ "$HADOOP_PROVIDED" == "true" ]; then
+    	 TGZ_VERSION=`echo $VERSION | sed -e 's/-SNAPSHOT$/-without-hadoop-SNAPSHOT/g'`
+    else
+         TGZ_VERSION=`echo $VERSION | sed -e 's/-SNAPSHOT$/-'$SPARK_HADOOP_VERSION'-SNAPSHOT/g'`
+    fi
 else
-	TGZ_VERSION="${VERSION}"
+    if [ "$HADOOP_PROVIDED" == "true" ]; then
+    	 TGZ_VERSION="${VERSION}-without-hadoop"
+    else
+         TGZ_VERSION="${VERSION}"
+    fi
 fi
 
 SPARK_DISTRIBUTION_FILE_NAME="spark-distribution_$SCALA_VERSION-$TGZ_VERSION.tgz"
