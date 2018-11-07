3c52163435e40ba090ed44fef7e989e0a895dcf9
zhou-jiang <zhou_jiang@apple.com>
2018-11-07 14:49:29 +0000

    SparkUI patch

--- core/src/main/scala/org/apache/spark/ui/SparkUI.scala
+++ core/src/main/scala/org/apache/spark/ui/SparkUI.scala
@@ -64,7 +64,7 @@ private[spark] class SparkUI private (
     attachTab(stagesTab)
     attachTab(new StorageTab(this, store))
     attachTab(new EnvironmentTab(this, store))
-    attachTab(new ExecutorsTab(this))
+    attachTab(new ExecutorsTab(this, store))
     addStaticHandler(SparkUI.STATIC_RESOURCE_DIR)
     attachHandler(createRedirectHandler("/", "/jobs/", basePath = basePath))
     attachHandler(ApiRootResource.getServletHandler(this))
@@ -149,6 +149,7 @@ private[spark] abstract class SparkUITab(parent: SparkUI, prefix: String)
   extends WebUITab(parent, prefix) {
 
   def appName: String = parent.appName
+  def appId: String = parent.appId
 
   def appSparkVersion: String = parent.appSparkVersion
 }
