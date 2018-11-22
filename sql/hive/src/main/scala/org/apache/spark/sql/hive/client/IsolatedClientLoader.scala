b836635ae37ce7b63e7ab551d9f56443c15c7cf0
Takanobu Asanuma <tasanuma@apache.org>
2018-11-21 23:09:57 -0800

    [SPARK-26134][CORE] Upgrading Hadoop to 2.7.4 to fix java.version problem
    When I ran spark-shell on JDK11+28(2018-09-25), It failed with the error below.
    
    ```
    Exception in thread "main" java.lang.ExceptionInInitializerError
    	at org.apache.hadoop.util.StringUtils.<clinit>(StringUtils.java:80)
    	at org.apache.hadoop.security.SecurityUtil.getAuthenticationMethod(SecurityUtil.java:611)
    	at org.apache.hadoop.security.UserGroupInformation.initialize(UserGroupInformation.java:273)
    	at org.apache.hadoop.security.UserGroupInformation.ensureInitialized(UserGroupInformation.java:261)
    	at org.apache.hadoop.security.UserGroupInformation.loginUserFromSubject(UserGroupInformation.java:791)
    	at org.apache.hadoop.security.UserGroupInformation.getLoginUser(UserGroupInformation.java:761)
    	at org.apache.hadoop.security.UserGroupInformation.getCurrentUser(UserGroupInformation.java:634)
    	at org.apache.spark.util.Utils$.$anonfun$getCurrentUserName$1(Utils.scala:2427)
    	at scala.Option.getOrElse(Option.scala:121)
    	at org.apache.spark.util.Utils$.getCurrentUserName(Utils.scala:2427)
    	at org.apache.spark.SecurityManager.<init>(SecurityManager.scala:79)
    	at org.apache.spark.deploy.SparkSubmit.secMgr$lzycompute$1(SparkSubmit.scala:359)
    	at org.apache.spark.deploy.SparkSubmit.secMgr$1(SparkSubmit.scala:359)
    	at org.apache.spark.deploy.SparkSubmit.$anonfun$prepareSubmitEnvironment$9(SparkSubmit.scala:367)
    	at scala.Option.map(Option.scala:146)
    	at org.apache.spark.deploy.SparkSubmit.prepareSubmitEnvironment(SparkSubmit.scala:367)
    	at org.apache.spark.deploy.SparkSubmit.submit(SparkSubmit.scala:143)
    	at org.apache.spark.deploy.SparkSubmit.doSubmit(SparkSubmit.scala:86)
    	at org.apache.spark.deploy.SparkSubmit$$anon$2.doSubmit(SparkSubmit.scala:927)
    	at org.apache.spark.deploy.SparkSubmit$.main(SparkSubmit.scala:936)
    	at org.apache.spark.deploy.SparkSubmit.main(SparkSubmit.scala)
    Caused by: java.lang.StringIndexOutOfBoundsException: begin 0, end 3, length 2
    	at java.base/java.lang.String.checkBoundsBeginEnd(String.java:3319)
    	at java.base/java.lang.String.substring(String.java:1874)
    	at org.apache.hadoop.util.Shell.<clinit>(Shell.java:52)
    ```
    This is a Hadoop issue that fails to parse some java.version. It has been fixed from Hadoop-2.7.4(see [HADOOP-14586](https://issues.apache.org/jira/browse/HADOOP-14586)).
    
    Note, Hadoop-2.7.5 or upper have another problem with Spark ([SPARK-25330](https://issues.apache.org/jira/browse/SPARK-25330)). So upgrading to 2.7.4 would be fine for now.
    
    Existing tests.
    
    Closes #23101 from tasanuma/SPARK-26134.
    
    Authored-by: Takanobu Asanuma <tasanuma@apache.org>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/client/IsolatedClientLoader.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/client/IsolatedClientLoader.scala
@@ -53,7 +53,7 @@ private[hive] object IsolatedClientLoader extends Logging {
       sharesHadoopClasses: Boolean = true): IsolatedClientLoader = synchronized {
     val resolvedVersion = hiveVersion(hiveMetastoreVersion)
     // We will first try to share Hadoop classes. If we cannot resolve the Hadoop artifact
-    // with the given version, we will use Hadoop 2.6 and then will not share Hadoop classes.
+    // with the given version, we will use Hadoop 2.7 and then will not share Hadoop classes.
     var _sharesHadoopClasses = sharesHadoopClasses
     val files = if (resolvedVersions.contains((resolvedVersion, hadoopVersion))) {
       resolvedVersions((resolvedVersion, hadoopVersion))
@@ -66,12 +66,12 @@ private[hive] object IsolatedClientLoader extends Logging {
             // If the error message contains hadoop, it is probably because the hadoop
             // version cannot be resolved.
             logWarning(s"Failed to resolve Hadoop artifacts for the version $hadoopVersion. " +
-              s"We will change the hadoop version from $hadoopVersion to 2.6.0 and try again. " +
+              s"We will change the hadoop version from $hadoopVersion to 2.7.4 and try again. " +
               "Hadoop classes will not be shared between Spark and Hive metastore client. " +
               "It is recommended to set jars used by Hive metastore client through " +
               "spark.sql.hive.metastore.jars in the production environment.")
             _sharesHadoopClasses = false
-            (downloadVersion(resolvedVersion, "2.6.5", ivyPath), "2.6.5")
+            (downloadVersion(resolvedVersion, "2.7.4", ivyPath), "2.7.4")
         }
       resolvedVersions.put((resolvedVersion, actualHadoopVersion), downloadedFiles)
       resolvedVersions((resolvedVersion, actualHadoopVersion))
