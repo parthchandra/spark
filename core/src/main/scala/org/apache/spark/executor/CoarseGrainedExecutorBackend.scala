47cdf6c4d8d607b9a76fc994be88cbd70474fc1c
nishchal <nishchal@apple.com>
2019-03-01 11:03:29 -0800

    Use Utils.localHostName() for bindAddress

--- core/src/main/scala/org/apache/spark/executor/CoarseGrainedExecutorBackend.scala
+++ core/src/main/scala/org/apache/spark/executor/CoarseGrainedExecutorBackend.scala
@@ -237,7 +237,7 @@ private[spark] object CoarseGrainedExecutorBackend extends Logging {
   def main(args: Array[String]) {
     var driverUrl: String = null
     var executorId: String = null
-    var bindAddress: String = null
+    var bindAddress: String = Utils.localHostName()
     var hostname: String = null
     var cores: Int = 0
     var appId: String = null
@@ -289,6 +289,9 @@ private[spark] object CoarseGrainedExecutorBackend extends Logging {
     if (bindAddress == null) {
       bindAddress = hostname
     }
+
+    logInfo("SPARK_LOCAL_HOSTNAME set to " + sys.env.get("SPARK_LOCAL_HOSTNAME"))
+    logInfo("Using bindAddress: " + bindAddress)
 
     run(driverUrl, executorId, bindAddress, hostname, cores, appId, workerUrl, userClassPath)
     System.exit(0)
