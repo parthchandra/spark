bc77923745189b1b6e04274ea28a2b93a2ddff4c
Nandor Kollar <nkollar@cloudera.com>
2019-03-06 14:11:58 +0100

    Parquet DELTA_BYTE_ARRAY is not supported in Spark 2.x's Vectorized Reader

--- sql/core/src/main/java/org/apache/spark/sql/execution/datasources/parquet/VectorizedColumnReader.java
+++ sql/core/src/main/java/org/apache/spark/sql/execution/datasources/parquet/VectorizedColumnReader.java
@@ -166,7 +166,7 @@ public class VectorizedColumnReader {
         readState.resetForNewPage(pageValueCount, pageFirstRowIndex);
       }
       PrimitiveType.PrimitiveTypeName typeName =
-          descriptor.getPrimitiveType().getPrimitiveTypeName();
+        descriptor.getPrimitiveType().getPrimitiveTypeName();
       if (isCurrentPageDictionaryEncoded) {
         // Save starting offset in case we need to decode dictionary IDs.
         int startOffset = readState.offset;
@@ -266,10 +266,7 @@ public class VectorizedColumnReader {
       this.dataColumn = new VectorizedRleValuesReader();
       this.isCurrentPageDictionaryEncoded = true;
     } else {
-      if (dataEncoding != Encoding.PLAIN) {
-        throw new UnsupportedOperationException("Unsupported encoding: " + dataEncoding);
-      }
-      this.dataColumn = new VectorizedPlainValuesReader();
+      this.dataColumn = getValuesReader(dataEncoding);
       this.isCurrentPageDictionaryEncoded = false;
     }
 
@@ -279,6 +276,20 @@ public class VectorizedColumnReader {
       throw new IOException("could not read page in col " + descriptor, e);
     }
   }
+    
+    private ValuesReader getValuesReader(Encoding encoding) {
+        switch (encoding) {
+            case PLAIN:
+                return new VectorizedPlainValuesReader();
+            case DELTA_BYTE_ARRAY:
+                return new VectorizedDeltaByteArrayReader();
+            case DELTA_BINARY_PACKED:
+                return new VectorizedDeltaBinaryPackedReader();
+            default:
+                throw new UnsupportedOperationException("Unsupported encoding: " + encoding);
+        }
+    }
+
 
   private int readPageV1(DataPageV1 page) throws IOException {
     if (page.getDlEncoding() != Encoding.RLE && descriptor.getMaxDefinitionLevel() != 0) {
