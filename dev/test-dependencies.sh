5d1981d2eaed6d1ab155dcb71ad6c5c29cf7a01f
Yuming Wang <yumwang@ebay.com>
2019-03-16 19:42:05 -0500

    [SPARK-27175][BUILD] Upgrade hadoop-3 to 3.2.0
    This PR upgrade `hadoop-3` to `3.2.0`  to workaround [HADOOP-16086](https://issues.apache.org/jira/browse/HADOOP-16086). Otherwise some test case will throw IllegalArgumentException:
    ```java
    02:44:34.707 ERROR org.apache.hadoop.hive.ql.exec.Task: Job Submission failed with exception 'java.io.IOException(Cannot initialize Cluster. Please check your configuration for mapreduce.framework.name and the correspond server addresses.)'
    java.io.IOException: Cannot initialize Cluster. Please check your configuration for mapreduce.framework.name and the correspond server addresses.
    	at org.apache.hadoop.mapreduce.Cluster.initialize(Cluster.java:116)
    	at org.apache.hadoop.mapreduce.Cluster.<init>(Cluster.java:109)
    	at org.apache.hadoop.mapreduce.Cluster.<init>(Cluster.java:102)
    	at org.apache.hadoop.mapred.JobClient.init(JobClient.java:475)
    	at org.apache.hadoop.mapred.JobClient.<init>(JobClient.java:454)
    	at org.apache.hadoop.hive.ql.exec.mr.ExecDriver.execute(ExecDriver.java:369)
    	at org.apache.hadoop.hive.ql.exec.mr.MapRedTask.execute(MapRedTask.java:151)
    	at org.apache.hadoop.hive.ql.exec.Task.executeTask(Task.java:199)
    	at org.apache.hadoop.hive.ql.exec.TaskRunner.runSequential(TaskRunner.java:100)
    	at org.apache.hadoop.hive.ql.Driver.launchTask(Driver.java:2183)
    	at org.apache.hadoop.hive.ql.Driver.execute(Driver.java:1839)
    	at org.apache.hadoop.hive.ql.Driver.runInternal(Driver.java:1526)
    	at org.apache.hadoop.hive.ql.Driver.run(Driver.java:1237)
    	at org.apache.hadoop.hive.ql.Driver.run(Driver.java:1227)
    	at org.apache.spark.sql.hive.client.HiveClientImpl.$anonfun$runHive$1(HiveClientImpl.scala:730)
    	at org.apache.spark.sql.hive.client.HiveClientImpl.$anonfun$withHiveState$1(HiveClientImpl.scala:283)
    	at org.apache.spark.sql.hive.client.HiveClientImpl.liftedTree1$1(HiveClientImpl.scala:221)
    	at org.apache.spark.sql.hive.client.HiveClientImpl.retryLocked(HiveClientImpl.scala:220)
    	at org.apache.spark.sql.hive.client.HiveClientImpl.withHiveState(HiveClientImpl.scala:266)
    	at org.apache.spark.sql.hive.client.HiveClientImpl.runHive(HiveClientImpl.scala:719)
    	at org.apache.spark.sql.hive.client.HiveClientImpl.runSqlHive(HiveClientImpl.scala:709)
    	at org.apache.spark.sql.hive.StatisticsSuite.createNonPartitionedTable(StatisticsSuite.scala:719)
    	at org.apache.spark.sql.hive.StatisticsSuite.$anonfun$testAlterTableProperties$2(StatisticsSuite.scala:822)
    ```
    
    manual tests
    
    Closes #24106 from wangyum/SPARK-27175.
    
    Authored-by: Yuming Wang <yumwang@ebay.com>
    Signed-off-by: Sean Owen <sean.owen@databricks.com>

--- dev/test-dependencies.sh
+++ dev/test-dependencies.sh
@@ -33,7 +33,7 @@ MVN="build/mvn"
 HADOOP_PROFILES=(
     hadoop-2.6
     hadoop-2.7
-    hadoop-3.1
+    hadoop-3.2
 )
 
 SCALA_VERSION=$("$MVN" help:evaluate -Dexpression=scala.binary.version $@ 2>/dev/null\
