abbaef8c509c74fbf823b44a744c012a26bbf001
zhou-jiang <zhou_jiang@apple.com>
2019-04-30 11:35:43 -0700

    <rdar://problem/49703974> [Apache Spark] URL.setURLStreamHandlerFactory causing incompatible with Cassandra Bulk Writer

--- sql/core/src/main/scala/org/apache/spark/sql/internal/SharedState.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/internal/SharedState.scala
@@ -42,6 +42,19 @@ import org.apache.spark.util.{MutableURLClassLoader, Utils}
  */
 private[sql] class SharedState(val sparkContext: SparkContext) extends Logging {
 
+  private var initialized = false
+
+  initialize()
+
+  private def initialize() {
+    synchronized {
+      if (!initialized) {
+        SharedState.setUrlStreamHanderFactoryIfNeeded(sparkContext.conf)
+        initialized = true
+      }
+    }
+  }
+
   // Load hive-site.xml into hadoopConf and determine the warehouse path we want to use, based on
   // the config from both hive and Spark SQL. Finally set the warehouse config value to sparkConf.
   val warehousePath: String = {
@@ -156,11 +169,16 @@ private[sql] class SharedState(val sparkContext: SparkContext) extends Logging {
 }
 
 object SharedState extends Logging {
-  try {
-    URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory())
-  } catch {
-    case e: Error =>
-      logWarning("URL.setURLStreamHandlerFactory failed to set FsUrlStreamHandlerFactory")
+
+  private def setUrlStreamHanderFactoryIfNeeded(conf: SparkConf) = {
+    try {
+      if (conf.getBoolean("spark.FsUrlStreamHandlerFactory.enabled", true)) {
+        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory())
+      }
+    } catch {
+      case e: Error =>
+        logWarning("URL.setURLStreamHandlerFactory failed to set FsUrlStreamHandlerFactory")
+    }
   }
 
   private val HIVE_EXTERNAL_CATALOG_CLASS_NAME = "org.apache.spark.sql.hive.HiveExternalCatalog"
