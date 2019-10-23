c84aa4994340a28906fc3832f164aa4252a26e84
Shruti Gumma <shruti_gumma@apple.com>
2019-10-23 16:11:11 -0700

    Updated make-distribution as examples module was removed

--- dev/make-distribution.sh
+++ dev/make-distribution.sh
@@ -230,20 +230,20 @@ if [ -d "$SPARK_HOME"/resource-managers/kubernetes/core/target/ ]; then
 fi
 
 # Copy examples and dependencies
-mkdir -p "$DISTDIR/examples/jars"
-cp "$SPARK_HOME"/examples/target/scala*/jars/* "$DISTDIR/examples/jars"
-
-# Deduplicate jars that have already been packaged as part of the main Spark dependencies.
-for f in "$DISTDIR"/examples/jars/*; do
-  name=$(basename "$f")
-  if [ -f "$DISTDIR/jars/$name" ]; then
-    rm "$DISTDIR/examples/jars/$name"
-  fi
-done
-
-# Copy example sources (needed for python and SQL)
-mkdir -p "$DISTDIR/examples/src/main"
-cp -r "$SPARK_HOME/examples/src/main" "$DISTDIR/examples/src/"
+#mkdir -p "$DISTDIR/examples/jars"
+##cp "$SPARK_HOME"/examples/target/scala*/jars/* "$DISTDIR/examples/jars"
+##
+### Deduplicate jars that have already been packaged as part of the main Spark dependencies.
+##for f in "$DISTDIR"/examples/jars/*; do
+##  name=$(basename "$f")
+##  if [ -f "$DISTDIR/jars/$name" ]; then
+##    rm "$DISTDIR/examples/jars/$name"
+##  fi
+##done
+##
+### Copy example sources (needed for python and SQL)
+##mkdir -p "$DISTDIR/examples/src/main"
+##cp -r "$SPARK_HOME/examples/src/main" "$DISTDIR/examples/src/"
 
 # Copy license and ASF files
 if [ -e "$SPARK_HOME/LICENSE-binary" ]; then
