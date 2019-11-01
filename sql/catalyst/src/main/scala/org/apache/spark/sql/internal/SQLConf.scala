744a645728c131436ab59b6412b3a5008b8d0e91
Miguel Nunes De Miranda <miguelnmiranda@apple.com>
2019-11-01 13:01:53 -0700

    [SPARK-17767][SQL] Support user-provided ExternalCatalog and SessionBuilder
    rdar://69319047 Spark should reject invalid CatalogImplementation Configuration
    
    Fail fast when SessionStateBuilder is set but not used.
    
    There are several ways configuration can be incorrect and we can cover more of them with
    fast failure rather than hitting strange errors later.
    
    Yes, previously if SessionStateBuilder was set but not used, no error would be thrown. Now it is invalid
    to have a configuration with this parameter set but not used.
    
    New unit test added

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -2999,6 +2999,20 @@ object SQLConf {
       .booleanConf
       .createWithDefault(false)
 
+  val SESSION_STATE_BUILDER_CLASS_NAME = buildConf("spark.sql.sessionStateBuilder")
+    .doc("Name of the class used to configure Spark SessionState builder. The class should " +
+      "extend BaseSessionStateBuilder.")
+    .internal()
+    .stringConf
+    .createOptional
+
+  val EXTERNAL_CATALOG_CLASS_NAME = buildConf("spark.sql.externalCatalog")
+    .doc("Name of the class used to configure Spark External Catalog. The class should " +
+      "implement ExternalCatalog interface.")
+    .internal()
+    .stringConf
+    .createOptional
+
   /**
    * Holds information about keys that have been deprecated.
    *
