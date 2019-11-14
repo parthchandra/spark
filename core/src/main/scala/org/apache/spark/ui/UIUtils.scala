cacb2fd6e59b42c2530cc3b9bd8fe68ff97edf14
Thiruvasakan Paramasivan <thiru@apple.com>
2019-11-13 16:42:54 -0800

    Use abstract storage as a replacement for window.localStorage

--- core/src/main/scala/org/apache/spark/ui/UIUtils.scala
+++ core/src/main/scala/org/apache/spark/ui/UIUtils.scala
@@ -175,6 +175,8 @@ private[spark] object UIUtils extends Logging {
     <link rel="stylesheet" href={prependBaseUri(request, "/static/webui.css")} type="text/css"/>
     <link rel="stylesheet"
           href={prependBaseUri(request, "/static/timeline-view.css")} type="text/css"/>
+    <script src={prependBaseUri(request, "/static/memorystorage.umd-0.11.0.js")} ></script>
+    <script src={prependBaseUri(request, "/static/storage.js")} ></script>
     <script src={prependBaseUri(request, "/static/sorttable.js")} ></script>
     <script src={prependBaseUri(request, "/static/jquery-1.12.4.min.js")}></script>
     <script src={prependBaseUri(request, "/static/vis.min.js")}></script>
