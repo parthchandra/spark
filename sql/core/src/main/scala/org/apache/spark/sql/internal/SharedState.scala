a6cb4a3cc063ac2d3c9bfcfbb357eded1459885f
HyukjinKwon <gurwls223@apache.org>
2019-11-19 09:08:20 +0900

    [SPARK-25694][SQL][FOLLOW-UP] Move 'spark.sql.defaultUrlStreamHandlerFactory.enabled' into StaticSQLConf.scala
    This PR is a followup of https://github.com/apache/spark/pull/26530 and proposes to move the configuration `spark.sql.defaultUrlStreamHandlerFactory.enabled` to `StaticSQLConf.scala` for consistency.
    
    To put the similar configurations together and for readability.
    
    No.
    
    Manually tested as described in https://github.com/apache/spark/pull/26530.
    
    Closes #26570 from HyukjinKwon/SPARK-25694.
    
    Authored-by: HyukjinKwon <gurwls223@apache.org>
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>
    (cherry picked from commit 8469614c0513fbed87977d4e741649db3fdd8add)
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- sql/core/src/main/scala/org/apache/spark/sql/internal/SharedState.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/internal/SharedState.scala
@@ -34,8 +34,6 @@ import org.apache.spark.sql.execution.CacheManager
 import org.apache.spark.sql.execution.ui.{SQLAppStatusListener, SQLAppStatusStore, SQLTab}
 import org.apache.spark.sql.internal.SQLConf.EXTERNAL_CATALOG_CLASS_NAME
 import org.apache.spark.sql.internal.StaticSQLConf._
-import org.apache.spark.sql.internal.config.DEFAULT_URL_STREAM_HANDLER_FACTORY_ENABLED
-import org.apache.spark.sql.streaming.StreamingQuery
 import org.apache.spark.status.ElementTrackingStore
 import org.apache.spark.util.{MutableURLClassLoader, Utils}
 
