83966b6568c7538b77480d449ad10d87d80ea04b
Dongjoon Hyun <dhyun@apple.com>
2019-12-13 14:15:39 -0800

    Revert "SparkUI Port value is not getting reloaded for StreamingContexts created from Checkpoint"
    This reverts commit 090d060b158052554feba1485719160e7ebf8894.

--- streaming/src/main/scala/org/apache/spark/streaming/Checkpoint.scala
+++ streaming/src/main/scala/org/apache/spark/streaming/Checkpoint.scala
@@ -65,11 +65,7 @@ class Checkpoint(ssc: StreamingContext, val checkpointTime: Time)
       "spark.kerberos.keytab",
       "spark.kerberos.principal",
       UI_FILTERS.key,
-      "spark.mesos.driver.frameworkId",
-      "spark.ui.port",
-      "spark.blockManager.port",
-      "spark.broadcast.port",
-      "spark.fileserver.port")
+      "spark.mesos.driver.frameworkId")
 
     val newSparkConf = new SparkConf(loadDefaults = false).setAll(sparkConfPairs)
       .remove("spark.driver.host")
