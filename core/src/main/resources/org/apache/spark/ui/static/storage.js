89f20cf4427a4f4987602db4cd514a7f7d70c041
Shruti Gumma <shruti_gumma@apple.com>
2020-01-03 12:39:25 -0800

    Updated state save parameter to rely on ajax enabled property

--- core/src/main/resources/org/apache/spark/ui/static/storage.js
+++ core/src/main/resources/org/apache/spark/ui/static/storage.js
@@ -1,4 +1,5 @@
 var DB;
+var isLocalStorageAvailable;
 try {
   var x = 'test_localstorage_available_' + Date.now();
   localStorage.setItem(x, x);
@@ -6,6 +7,8 @@ try {
   localStorage.removeItem(x);
   if (x !== y) {throw new Error();}
   DB = localStorage;
+  isLocalStorageAvailable = true;
 } catch (exception) {
   DB = new MemoryStorage('spark');
+  isLocalStorageAvailable = false;
 }
