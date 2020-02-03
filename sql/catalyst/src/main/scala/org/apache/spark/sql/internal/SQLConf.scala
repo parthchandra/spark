c6a9ee37885c32892383761986a3aaeb31041fba
Yuanjian Li <xyliyuanjian@gmail.com>
2020-02-02 23:37:13 -0800

    [SPARK-29543][SS][FOLLOWUP] Move `spark.sql.streaming.ui.*` configs to StaticSQLConf
    ### What changes were proposed in this pull request?
    Put the configs below needed by Structured Streaming UI into StaticSQLConf:
    
    - spark.sql.streaming.ui.enabled
    - spark.sql.streaming.ui.retainedProgressUpdates
    - spark.sql.streaming.ui.retainedQueries
    
    ### Why are the changes needed?
    Make all SS UI configs consistent with other similar configs in usage and naming.
    
    ### Does this PR introduce any user-facing change?
    Yes, add new static config `spark.sql.streaming.ui.retainedProgressUpdates`.
    
    ### How was this patch tested?
    Existing UT.
    
    Closes #27425 from xuanyuanking/SPARK-29543-follow.
    
    Authored-by: Yuanjian Li <xyliyuanjian@gmail.com>
    Signed-off-by: Shixiong Zhu <zsxwing@gmail.com>
    (cherry picked from commit a4912cee615314e9578e6ab4eae25f147feacbd5)
    Signed-off-by: Shixiong Zhu <zsxwing@gmail.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -1164,18 +1164,6 @@ object SQLConf {
       .booleanConf
       .createWithDefault(true)
 
-  val STREAMING_UI_ENABLED =
-    buildConf("spark.sql.streaming.ui.enabled")
-      .doc("Whether to run the structured streaming UI for the Spark application.")
-      .booleanConf
-      .createWithDefault(true)
-
-  val STREAMING_UI_INACTIVE_QUERY_RETENTION =
-    buildConf("spark.sql.streaming.ui.numInactiveQueries")
-      .doc("The number of inactive queries to retain for structured streaming ui.")
-      .intConf
-      .createWithDefault(100)
-
   val VARIABLE_SUBSTITUTE_ENABLED =
     buildConf("spark.sql.variable.substitute")
       .doc("This enables substitution using syntax like ${var} ${system:var} and ${env:var}.")
@@ -2297,10 +2285,6 @@ class SQLConf extends Serializable with Logging {
   def checkpointLocation: Option[String] = getConf(CHECKPOINT_LOCATION)
 
   def isUnsupportedOperationCheckEnabled: Boolean = getConf(UNSUPPORTED_OPERATION_CHECK_ENABLED)
-
-  def isStreamingUIEnabled: Boolean = getConf(STREAMING_UI_ENABLED)
-
-  def streamingUIInactiveQueryRetention: Int = getConf(STREAMING_UI_INACTIVE_QUERY_RETENTION)
 
   def streamingFileCommitProtocolClass: String = getConf(STREAMING_FILE_COMMIT_PROTOCOL_CLASS)
 
