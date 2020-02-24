e6b577553170db39e7b880af3b68eaf908c8134f
Dongjoon Hyun <dhyun@apple.com>
2020-02-23 19:21:23 -0800

    [MINOR] Switch hive-metastore to 2.3.6.1-apple

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -228,6 +228,7 @@ object SparkBuild extends PomBuild {
       // See https://storage-download.googleapis.com/maven-central/index.html for more info.
       "gcs-maven-central-mirror" at "https://maven-central.storage-download.googleapis.com/repos/central/data/",
       DefaultMavenRepository,
+      "apple" at "https://artifacts.apple.com/libs-release",
       Resolver.mavenLocal,
       Resolver.file("local", file(Path.userHome.absolutePath + "/.ivy2/local"))(Resolver.ivyStylePatterns)
     ),
