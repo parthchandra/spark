219d50f44dc63e87f73e45b2a4dd067fb06fbd60
Peter Toth <peter.toth@gmail.com>
2020-02-24 13:46:21 -0800

    [SPARK-30870][SQL] Column pruning shouldn't alias a nested column if it means the whole structure
    This PR fixes a bug in nested column aliasing by taking the data type of the referenced nested fields into account when calculating the number of extracted columns. After this PR this query runs without issues:
    ```
    SELECT explodedvalue.*
    FROM VALUES array(named_struct('nested', named_struct('a', 1, 'b', 2))) AS (value)
    LATERAL VIEW explode(value) AS explodedvalue
    ```
    This is a regression from Spark 2.4.
    
    To fix a bug.
    
    No.
    
    Added new UT.
    
    Closes #27675 from peter-toth/SPARK-30870.
    
    Authored-by: Peter Toth <peter.toth@gmail.com>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>
    (cherry picked from commit 1a4e2423b284d8382fcf9547d4351b20d7a2be56)
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>

--- sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
@@ -3102,6 +3102,17 @@ class SQLQuerySuite extends QueryTest with SharedSQLContext {
         "JOIN ( SELECT * FROM B JOIN C USING (id)) USING (id)"), Row(0))
     }
   }
+
+  test("SPARK-30870: Column pruning shouldn't alias a nested column if it means the whole " +
+    "structure") {
+    val df = sql(
+      """
+        |SELECT explodedvalue.field
+        |FROM VALUES array(named_struct('field', named_struct('a', 1, 'b', 2))) AS (value)
+        |LATERAL VIEW explode(value) AS explodedvalue
+      """.stripMargin)
+    checkAnswer(df, Row(Row(1, 2)) :: Nil)
+  }
 }
 
 case class Foo(bar: Option[String])
