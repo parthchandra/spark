10ad8b32a9f56e0c1fec79efaae560b53e0566fd
Dongjoon Hyun <dongjoon@apache.org>
2020-03-12 09:06:29 -0700

    [SPARK-31130][BUILD] Use the same version of `commons-io` in SBT
    This PR (SPARK-31130) aims to pin `Commons IO` version to `2.4` in SBT build like Maven build.
    
    [HADOOP-15261](https://issues.apache.org/jira/browse/HADOOP-15261) upgraded `commons-io` from 2.4 to 2.5 at Apache Hadoop 3.1.
    
    In `Maven`, Apache Spark always uses `Commons IO 2.4` based on `pom.xml`.
    ```
    $ git grep commons-io.version
    pom.xml:    <commons-io.version>2.4</commons-io.version>
    pom.xml:        <version>${commons-io.version}</version>
    ```
    
    However, `SBT` choose `2.5`.
    
    **branch-3.0**
    ```
    $ build/sbt -Phadoop-3.2 "core/dependencyTree" | grep commons-io:commons-io | head -n1
    [info]   | | +-commons-io:commons-io:2.5
    ```
    
    **branch-2.4**
    ```
    $ build/sbt -Phadoop-3.1 "core/dependencyTree" | grep commons-io:commons-io | head -n1
    [info]   | | +-commons-io:commons-io:2.5
    ```
    
    No.
    
    Pass the Jenkins with `[test-hadoop3.2]` (the default PR Builder is `SBT`) and manually do the following locally.
    ```
    build/sbt -Phadoop-3.2 "core/dependencyTree" | grep commons-io:commons-io | head -n1
    ```
    
    Closes #27886 from dongjoon-hyun/SPARK-31130.
    
    Authored-by: Dongjoon Hyun <dongjoon@apache.org>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>
    (cherry picked from commit 972e23d18186c73026ebed95b37a886ca6eecf3e)
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -552,6 +552,7 @@ object DockerIntegrationTests {
 object DependencyOverrides {
   lazy val settings = Seq(
     dependencyOverrides += "com.google.guava" % "guava" % "14.0.1",
+    dependencyOverrides += "commons-io" % "commons-io" % "2.4",
     dependencyOverrides += "com.fasterxml.jackson.core"  % "jackson-annotations" % "2.6.7",
     dependencyOverrides += "com.fasterxml.jackson.core"  % "jackson-core" % "2.6.7",
     dependencyOverrides += "com.fasterxml.jackson.core"  % "jackson-databind" % "2.6.7.3",
