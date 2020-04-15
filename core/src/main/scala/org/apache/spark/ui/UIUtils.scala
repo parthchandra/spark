4bb922d98a5a99572755f40e217cbe21c168091f
Kousuke Saruta <sarutak@oss.nttdata.com>
2020-04-14 21:28:02 -0700

    [SPARK-31420][WEBUI][3.0] Infinite timeline redraw in job details page
    ### What changes were proposed in this pull request?
    
    This PR backports #28192 to branch-3.0.
    
    ### Why are the changes needed?
    
    SPARK-31420 affects branch-3.0 too.
    
    ### Does this PR introduce any user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Same as #28192
    
    Closes #28213 from sarutak/SPARK-31420-branch-3.0.
    
    Authored-by: Kousuke Saruta <sarutak@oss.nttdata.com>
    Signed-off-by: Gengliang Wang <gengliang.wang@databricks.com>

--- core/src/main/scala/org/apache/spark/ui/UIUtils.scala
+++ core/src/main/scala/org/apache/spark/ui/UIUtils.scala
@@ -224,7 +224,8 @@ private[spark] object UIUtils extends Logging {
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
     <link rel="stylesheet"
           href={prependBaseUri(request, "/static/bootstrap.min.css")} type="text/css"/>
-    <link rel="stylesheet" href={prependBaseUri(request, "/static/vis.min.css")} type="text/css"/>
+    <link rel="stylesheet"
+          href={prependBaseUri(request, "/static/vis-timeline-graph2d.min.css")} type="text/css"/>
     <link rel="stylesheet" href={prependBaseUri(request, "/static/webui.css")} type="text/css"/>
     <link rel="stylesheet"
           href={prependBaseUri(request, "/static/timeline-view.css")} type="text/css"/>
@@ -232,7 +233,7 @@ private[spark] object UIUtils extends Logging {
     <script src={prependBaseUri(request, "/static/storage.js")} ></script>
     <script src={prependBaseUri(request, "/static/sorttable.js")} ></script>
     <script src={prependBaseUri(request, "/static/jquery-3.4.1.min.js")}></script>
-    <script src={prependBaseUri(request, "/static/vis.min.js")}></script>
+    <script src={prependBaseUri(request, "/static/vis-timeline-graph2d.min.js")}></script>
     <script src={prependBaseUri(request, "/static/bootstrap-tooltip.js")}></script>
     <script src={prependBaseUri(request, "/static/initialize-tooltips.js")}></script>
     <script src={prependBaseUri(request, "/static/table.js")}></script>
