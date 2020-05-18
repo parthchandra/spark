dcbfae1b92d6adc4e2204daa2edbdd998e735079
Dongjoon Hyun <dongjoon@apache.org>
2020-05-17 21:35:42 -0700

    [SPARK-31743][CORE] Add spark_info metric into PrometheusResource
    This PR aims to add `spark_info` metric into `PrometheusResource`.
    
    This exposes Apache Spark version and revision like the following.
    
    ![Screen Shot 2020-05-17 at 6 02 20 PM](https://user-images.githubusercontent.com/9700541/82165091-990ce000-9868-11ea-82d5-8ea344eef646.png)
    
    ![Screen Shot 2020-05-17 at 6 06 32 PM](https://user-images.githubusercontent.com/9700541/82165247-2cdeac00-9869-11ea-83aa-e8083fa12a92.png)
    
    Yes, but it's exposed as an additional metric.
    
    Manual.
    ```
    $ bin/spark-shell --driver-memory 4G -c spark.ui.prometheus.enabled=true
    
    $ curl -s http://localhost:4041/metrics/executors/prometheus/ | head -n1
    spark_info{version="3.1.0", revision="097d5098cca987e5f7bbb8394783c01517ebed0f"} 1.0
    ```
    
    Closes #28563 from dongjoon-hyun/SPARK-31743.
    
    Authored-by: Dongjoon Hyun <dongjoon@apache.org>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>
    (cherry picked from commit 64795f9e0c85a999bf808432d0d533843bea0a31)
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- core/src/main/scala/org/apache/spark/status/api/v1/PrometheusResource.scala
+++ core/src/main/scala/org/apache/spark/status/api/v1/PrometheusResource.scala
@@ -23,6 +23,8 @@ import org.eclipse.jetty.servlet.{ServletContextHandler, ServletHolder}
 import org.glassfish.jersey.server.ServerProperties
 import org.glassfish.jersey.servlet.ServletContainer
 
+import org.apache.spark.{SPARK_REVISION, SPARK_VERSION_SHORT}
+import org.apache.spark.annotation.Experimental
 import org.apache.spark.ui.SparkUI
 
 /**
@@ -39,6 +41,7 @@ private[v1] class PrometheusResource extends ApiRequestContext {
   @Produces(Array(MediaType.TEXT_PLAIN))
   def executors(): String = {
     val sb = new StringBuilder
+    sb.append(s"""spark_info{version="$SPARK_VERSION_SHORT", revision="$SPARK_REVISION"} 1.0\n""")
     val store = uiRoot.asInstanceOf[SparkUI].store
     store.executorList(true).foreach { executor =>
       val prefix = "metrics_executor_"
