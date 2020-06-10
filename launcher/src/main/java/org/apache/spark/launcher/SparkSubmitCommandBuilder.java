21085b82a2d03084fba11d37dca154860fc9bdc8
Dongjoon Hyun <dhyun@apple.com>
2020-06-09 21:19:40 -0700

    [MINOR] Add JDK11 options

--- launcher/src/main/java/org/apache/spark/launcher/SparkSubmitCommandBuilder.java
+++ launcher/src/main/java/org/apache/spark/launcher/SparkSubmitCommandBuilder.java
@@ -288,6 +288,19 @@ class SparkSubmitCommandBuilder extends AbstractCommandBuilder {
       cmd.add("-Xmx" + memory);
       addOptionString(cmd, driverDefaultJavaOptions);
       addOptionString(cmd, driverExtraJavaOptions);
+      int majorVersion = Integer.parseInt(System.getProperty("java.version").split("\\D+")[0]);
+      if (majorVersion >= 9) {
+        String JDK11_OPTIONS =
+          "--add-opens java.base/java.lang.invoke=ALL-UNNAMED " +
+          "--add-opens java.base/java.nio=ALL-UNNAMED " +
+          "--add-opens java.base/java.net=ALL-UNNAMED " +
+          "--add-opens java.base/java.util=ALL-UNNAMED " +
+          "--add-opens java.base/jdk.internal.misc=ALL-UNNAMED " +
+          "--add-opens java.base/sun.nio.ch=ALL-UNNAMED " +
+          "--add-opens java.security.jgss/sun.security.krb5=ALL-UNNAMED " +
+          "-Dio.netty.tryReflectionSetAccessible=true";
+        addOptionString(cmd, JDK11_OPTIONS);
+      }
       mergeEnvPathList(env, getLibPathEnvName(),
         config.get(SparkLauncher.DRIVER_EXTRA_LIBRARY_PATH));
     }
