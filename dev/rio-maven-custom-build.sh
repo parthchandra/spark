75bd794f1c9d83dff3c34bd9a33dacdb6f6fea8e
Holden Karau <hkarau@apple.com>
2020-08-04 10:47:45 -0700

    Try and see if we need to specify the TZ into the current.time

--- dev/rio-maven-custom-build.sh
+++ dev/rio-maven-custom-build.sh
@@ -129,8 +129,8 @@ fi
 cd "$SPARK_HOME"
 
 ##Set MAVEN_OPTS
-time_stamp=$(date '+%Y%m%d%H%M%S')
-export MAVEN_OPTS="${ADDITIONAL_MAVEN_OPTS} ${SKIP_TEST_PACKAGE_D_PARAM} $SKIP_TESTS_D_PARAM -Dscala-${SCALA_VERSION}=enabled -Dhive-thriftserver=enabled ${HADOOP_VERSION_D_PARAM} -DdeployAtEnd=true -DinstallAtEnd=true -Dcurrent.time=${time_stamp}"
+time_stamp=$(date '+%Y-%m-%d %H:%M:%S UTC')
+export MAVEN_OPTS="${ADDITIONAL_MAVEN_OPTS} ${SKIP_TEST_PACKAGE_D_PARAM} $SKIP_TESTS_D_PARAM -Dscala-${SCALA_VERSION}=enabled -Dhive-thriftserver=enabled ${HADOOP_VERSION_D_PARAM} -DdeployAtEnd=true -DinstallAtEnd=true -Dcurrent.time=\"${time_stamp}\""
 
 echo -e "MAVEN_OPTS exported: ${MAVEN_OPTS}"
 
