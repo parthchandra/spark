4c1cb2221899776fae6b19675bf9b584447a0c95
Holden Karau <hkarau@apple.com>
2020-08-04 21:26:58 -0700

    Fix timestamp rewrite since we pattern match on it apparently.

--- dev/rio-maven-custom-build.sh
+++ dev/rio-maven-custom-build.sh
@@ -151,11 +151,11 @@ execute_command "$MVN" clean deploy -DaltDeploymentRepository="${REPO_URL}" "$SK
 t=$(date '+%H%M%S')
 if [ $IS_RELEASE -eq 0 ] ; then
   find "./.dist/local-repo/org/apache/spark/" -name "*.jar" -print0 | while read -d $'\0' file; do
-    new_name=$(echo "$file" | sed -E  "s/-([[:digit:]]+)\.[[:digit:]]+-([[:digit:]]+)/-SNAPSHOT-\1-${t}-\2/")
+    new_name=$(echo "$file" | sed -E  "s/-([[:digit:]]+)\.[[:digit:]]+-([[:digit:]]+)/-SNAPSHOT-\1.${t}-\2/")
     mv "$file" "$new_name"
   done
   find "./.dist/local-repo/org/apache/spark/" -name "*.pom" -print0 | while read -d $'\0' file; do
-    new_name=$(echo "$file" | sed -E  "s/-([[:digit:]]+)\.[[:digit:]]+-([[:digit:]]+)/-SNAPSHOT-\1-${t}-\2/")
+    new_name=$(echo "$file" | sed -E  "s/-([[:digit:]]+)\.[[:digit:]]+-([[:digit:]]+)/-SNAPSHOT-\1.${t}-\2/")
     mv "$file" "$new_name"
   done
 fi
