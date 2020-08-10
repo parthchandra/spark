3bdc8ccdfccb17bdd25bda278948e99579c6ba88
Dongjoon Hyun <dongjoon@apache.org>
2020-08-10 07:33:06 -0700

    [SPARK-32517][CORE] Add StorageLevel.DISK_ONLY_3
    This PR aims to add `StorageLevel.DISK_ONLY_3` as a built-in `StorageLevel`.
    
    In a YARN cluster, HDFS uaually provides storages with replication factor 3. So, we can save the result to HDFS to get `StorageLevel.DISK_ONLY_3` technically. However, disaggregate clusters or clusters without storage services are rising. Previously, in that situation, the users were able to use similar `MEMORY_AND_DISK_2` or a user-created `StorageLevel`. This PR aims to support those use cases officially for better UX.
    
    Yes. This provides a new built-in option.
    
    Pass the GitHub Action or Jenkins with the revised test cases.
    
    Closes #29331 from dongjoon-hyun/SPARK-32517.
    
    Authored-by: Dongjoon Hyun <dongjoon@apache.org>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>
    (cherry picked from commit b421bf0196897fa66d6d15ba6461a24b23ac6dd3)
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>
    (cherry picked from commit 94174bfbcebc69c1d4ed976c3badfaa80508846a)
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>
    (cherry picked from commit 928067debb138aea0f6277c5b0f8e7e53a803437)
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>
    (cherry picked from commit 595327545b1d744cc79115c8a31fef9dcf207dc2)
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- core/src/test/scala/org/apache/spark/DistributedSuite.scala
+++ core/src/test/scala/org/apache/spark/DistributedSuite.scala
@@ -36,7 +36,7 @@ class DistributedSuite extends SparkFunSuite with Matchers with LocalSparkContex
   // Necessary to make ScalaTest 3.x interrupt a thread on the JVM like ScalaTest 2.2.x
   implicit val defaultSignaler: Signaler = ThreadSignaler
 
-  val clusterUrl = "local-cluster[2,1,1024]"
+  val clusterUrl = "local-cluster[3,1,1024]"
 
   test("task throws not serializable exception") {
     // Ensures that executors do not crash when an exn is not serializable. If executors crash,
@@ -172,7 +172,7 @@ class DistributedSuite extends SparkFunSuite with Matchers with LocalSparkContex
 
   private def testCaching(conf: SparkConf, storageLevel: StorageLevel): Unit = {
     sc = new SparkContext(conf.setMaster(clusterUrl).setAppName("test"))
-    TestUtils.waitUntilExecutorsUp(sc, 2, 30000)
+    TestUtils.waitUntilExecutorsUp(sc, 3, 30000)
     val data = sc.parallelize(1 to 1000, 10)
     val cachedData = data.persist(storageLevel)
     assert(cachedData.count === 1000)
@@ -204,7 +204,8 @@ class DistributedSuite extends SparkFunSuite with Matchers with LocalSparkContex
     "caching on disk" -> StorageLevel.DISK_ONLY,
     "caching in memory, replicated" -> StorageLevel.MEMORY_ONLY_2,
     "caching in memory, serialized, replicated" -> StorageLevel.MEMORY_ONLY_SER_2,
-    "caching on disk, replicated" -> StorageLevel.DISK_ONLY_2,
+    "caching on disk, replicated 2" -> StorageLevel.DISK_ONLY_2,
+    "caching on disk, replicated 3" -> StorageLevel.DISK_ONLY_3,
     "caching in memory and disk, replicated" -> StorageLevel.MEMORY_AND_DISK_2,
     "caching in memory and disk, serialized, replicated" -> StorageLevel.MEMORY_AND_DISK_SER_2
   ).foreach { case (testName, storageLevel) =>
