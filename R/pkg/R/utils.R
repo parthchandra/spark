928067debb138aea0f6277c5b0f8e7e53a803437
Dongjoon Hyun <dongjoon@apache.org>
2020-08-10 07:33:06 -0700

    [SPARK-32517][CORE] Add StorageLevel.DISK_ONLY_3
    This PR aims to add `StorageLevel.DISK_ONLY_3` as a built-in `StorageLevel`.
    
    In a YARN cluster, HDFS uaually provides storages with replication factor 3. So, we can save the result to HDFS to get `StorageLevel.DISK_ONLY_3` technically. However, disaggregate clusters or clusters without storage services are rising. Previously, in that situation, the users were able to use similar `MEMORY_AND_DISK_2` or a user-created `StorageLevel`. This PR aims to support those use cases officially for better UX.
    
    Yes. This provides a new built-in option.
    
    Pass the GitHub Action or Jenkins with the revised test cases.
    
    Closes #29331 from dongjoon-hyun/SPARK-32517.
    
    Authored-by: Dongjoon Hyun <dongjoon@apache.org>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>
    (cherry picked from commit b421bf0196897fa66d6d15ba6461a24b23ac6dd3)
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>
    (cherry picked from commit 94174bfbcebc69c1d4ed976c3badfaa80508846a)
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- R/pkg/R/utils.R
+++ R/pkg/R/utils.R
@@ -377,6 +377,7 @@ varargsToStrEnv <- function(...) {
 
 getStorageLevel <- function(newLevel = c("DISK_ONLY",
                                          "DISK_ONLY_2",
+                                         "DISK_ONLY_3",
                                          "MEMORY_AND_DISK",
                                          "MEMORY_AND_DISK_2",
                                          "MEMORY_AND_DISK_SER",
@@ -391,6 +392,7 @@ getStorageLevel <- function(newLevel = c("DISK_ONLY",
   storageLevel <- switch(newLevel,
                          "DISK_ONLY" = callJStatic(storageLevelClass, "DISK_ONLY"),
                          "DISK_ONLY_2" = callJStatic(storageLevelClass, "DISK_ONLY_2"),
+                         "DISK_ONLY_3" = callJStatic(storageLevelClass, "DISK_ONLY_3"),
                          "MEMORY_AND_DISK" = callJStatic(storageLevelClass, "MEMORY_AND_DISK"),
                          "MEMORY_AND_DISK_2" = callJStatic(storageLevelClass, "MEMORY_AND_DISK_2"),
                          "MEMORY_AND_DISK_SER" = callJStatic(storageLevelClass,
@@ -416,6 +418,8 @@ storageLevelToString <- function(levelObj) {
     "DISK_ONLY"
   } else if (useDisk && !useMemory && !useOffHeap && !deserialized && replication == 2) {
     "DISK_ONLY_2"
+  } else if (useDisk && !useMemory && !useOffHeap && !deserialized && replication == 3) {
+    "DISK_ONLY_3"
   } else if (!useDisk && useMemory && !useOffHeap && deserialized && replication == 1) {
     "MEMORY_ONLY"
   } else if (!useDisk && useMemory && !useOffHeap && deserialized && replication == 2) {
