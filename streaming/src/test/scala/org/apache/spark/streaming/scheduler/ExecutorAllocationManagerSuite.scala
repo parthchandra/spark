holdenk/spark-1

Squashed commits to add decommissioning to Spark's dynamic scaledown
Add an option to migrate shuffle blocks as well as the current cache blocks during decommissioning

Update core/src/main/scala/org/apache/spark/storage/BlockManager.scala

CR feedback "Nit: I think the comment is not needed as your code is self-explanatory here:"

Co-authored-by: Attila Zsolt Piros <2017933+attilapiros@users.noreply.github.com>

Update core/src/main/scala/org/apache/spark/shuffle/IndexShuffleBlockResolver.scala

If we have a failure during block migration, log the exception.

Co-authored-by: Attila Zsolt Piros <2017933+attilapiros@users.noreply.github.com>

First pass at the feedback from @attilapiros, mostly minor re-arrangement

Saw a test failure which could come from us not having a reasonable timeout here

Improve error logging

Co-authored-by: Attila Zsolt Piros <2017933+attilapiros@users.noreply.github.com>

cleanup

Co-authored-by: Attila Zsolt Piros <2017933+attilapiros@users.noreply.github.com>

cleanup

Co-authored-by: Attila Zsolt Piros <2017933+attilapiros@users.noreply.github.com>

Add more info to debugging

Co-authored-by: Attila Zsolt Piros <2017933+attilapiros@users.noreply.github.com>

logging string interpolation

Co-authored-by: Attila Zsolt Piros <2017933+attilapiros@users.noreply.github.com>

logging string interpolation

Co-authored-by: Attila Zsolt Piros <2017933+attilapiros@users.noreply.github.com>

logging string interpolation

Co-authored-by: Attila Zsolt Piros <2017933+attilapiros@users.noreply.github.com>

Generalize the decom put to check put as stream and shuffle blocks as well (help avoid cascading block migration)

spacing

Fix long line, make our shuffle block threads stop so we don't leak threads during testing.

Remove un-needed shuffleStatus.invalidateSerializedMapOutputStatusCache and log the scheduler when asked to decom and we can't

Always transfer shuffle blocks as put, take out the spark.network.maxRemoteBlockSizeFetchToMem test that we 

streaming/src/test/scala/org/apache/spark/streaming/scheduler/ExecutorAllocationManagerSuite.scala
@@ -43,6 +43,14 @@ class ExecutorAllocationManagerSuite extends TestSuiteBase
   }
 
   test("basic functionality") {
+    basicTest(decommissioning = false)
+  }
+
+  test("basic decommissioning") {
+    basicTest(decommissioning = true)
+  }
+
+  def basicTest(decommissioning: Boolean): Unit = {
     // Test that adding batch processing time info to allocation manager
     // causes executors to be requested and killed accordingly
 
@@ -77,12 +85,20 @@ class ExecutorAllocationManagerSuite extends TestSuiteBase
         }
       }
 
-      /** Verify that a particular executor was killed */
+      /** Verify that particular executors was killed */
       def verifyKilledExec(expectedKilledExec: Option[String]): Unit = {
         if (expectedKilledExec.nonEmpty) {
-          verify(allocationClient, times(1)).killExecutor(meq(expectedKilledExec.get))
+          if (decommissioning) {
+            verify(allocationClient, times(1)).decommissionExecutors(meq(expectedKilledExec.toSeq))
+          } else {
+            verify(allocationClient, times(1)).killExecutor(meq(expectedKilledExec.get))
+          }
         } else {
-          verify(allocationClient, never).killExecutor(null)
+          if (decommissioning) {
+            verify(allocationClient, never).decommissionExecutors(null)
+          } else {
+            verify(allocationClient, never).killExecutor(null)
+          }
         }
       }
 

parthchandra/spark

streaming/src/test/scala/org/apache/spark/streaming/scheduler/ExecutorAllocationManagerSuite.scala
@@ -27,6 +27,7 @@ import org.scalatestplus.mockito.MockitoSugar
 import org.apache.spark.{ExecutorAllocationClient, SparkConf}
 import org.apache.spark.internal.config.{DYN_ALLOCATION_ENABLED, DYN_ALLOCATION_TESTING}
 import org.apache.spark.internal.config.Streaming._
+import org.apache.spark.internal.config.Worker.WORKER_DECOMMISSION_ENABLED
 import org.apache.spark.streaming.{DummyInputDStream, Seconds, StreamingContext, TestSuiteBase}
 import org.apache.spark.util.{ManualClock, Utils}
 
@@ -43,11 +44,22 @@ class ExecutorAllocationManagerSuite extends TestSuiteBase
   }
 
   test("basic functionality") {
+    basicTest(decommissioning = false)
+  }
+
+  test("basic decommissioning") {
+    basicTest(decommissioning = true)
+  }
+
+  def basicTest(decommissioning: Boolean): Unit = {
     // Test that adding batch processing time info to allocation manager
     // causes executors to be requested and killed accordingly
+    conf.set(WORKER_DECOMMISSION_ENABLED, decommissioning)
 
     // There is 1 receiver, and exec 1 has been allocated to it
-    withAllocationManager(numReceivers = 1) { case (receiverTracker, allocationManager) =>
+    withAllocationManager(numReceivers = 1, conf = conf) {
+      case (receiverTracker, allocationManager) =>
+
       when(receiverTracker.allocatedExecutors).thenReturn(Map(1 -> Some("1")))
 
       /** Add data point for batch processing time and verify executor allocation */
@@ -77,12 +89,20 @@ class ExecutorAllocationManagerSuite extends TestSuiteBase
         }
       }
 
-      /** Verify that a particular executor was killed */
+      /** Verify that particular executors was killed */
       def verifyKilledExec(expectedKilledExec: Option[String]): Unit = {
         if (expectedKilledExec.nonEmpty) {
-          verify(allocationClient, times(1)).killExecutor(meq(expectedKilledExec.get))
+          if (decommissioning) {
+            verify(allocationClient, times(1)).decommissionExecutor(meq(expectedKilledExec.get))
+          } else {
+            verify(allocationClient, times(1)).killExecutor(meq(expectedKilledExec.get))
+          }
         } else {
-          verify(allocationClient, never).killExecutor(null)
+          if (decommissioning) {
+            verify(allocationClient, never).decommissionExecutors(null, false)
+          } else {
+            verify(allocationClient, never).killExecutor(null)
+          }
         }
       }
 