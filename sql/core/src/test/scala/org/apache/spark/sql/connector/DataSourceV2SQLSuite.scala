3bb8eed03b903434bc75ba53f8d26c15858553fa
Dongjoon Hyun <dongjoon@apache.org>
2020-12-09 20:10:40 -0800

    Fix UT failures due to `Rewrite DELETE commands`

--- sql/core/src/test/scala/org/apache/spark/sql/connector/DataSourceV2SQLSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/connector/DataSourceV2SQLSuite.scala
@@ -1863,7 +1863,7 @@ class DataSourceV2SQLSuite
       }
 
       assert(spark.table(t).count === 3)
-      assert(exc.getMessage.contains("Delete by condition with subquery is not supported"))
+      assert(exc.getMessage.contains("Table does not support updates and deletes"))
     }
   }
 
@@ -1877,7 +1877,7 @@ class DataSourceV2SQLSuite
       }
 
       assert(spark.table(t).count === 3)
-      assert(exc.getMessage.contains(s"Cannot delete from table $t"))
+      assert(exc.getMessage.contains("Table does not support updates and deletes"))
     }
   }
 
