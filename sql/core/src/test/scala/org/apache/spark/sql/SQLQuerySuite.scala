2fe875bdfea1e03a871b0056cc8f33006fd25dd5
Kent Yao <yaooqinn@hotmail.com>
2020-12-16 14:09:28 -0800

    [SPARK-33806][SQL] limit partition num to 1 when distributing by foldable expressions
    It seems a very popular way that people use DISTRIBUTE BY clause with a literal to coalesce partition in the pure SQL data processing.
    
    For example
    ```
    insert into table src select * from values (1), (2), (3) t(a) distribute by 1
    ```
    
    Users may want the final output to be one single data file, but if the reality is not always true. Spark will always create a file for partition 0 whether it contains data or not, so when the data all goes to a partition(IDX >0), there will be always 2 files there and the part-00000 is empty. On the other hand, a lot of empty tasks will be launched too, this is unnecessary.
    
    When users repeat the insert statement daily, hourly, or minutely, it causes small file issues.
    
    ```
    spark-sql> set spark.sql.shuffle.partitions=3;drop table if exists test2;create table test2 using parquet as select * from values (1), (2), (3) t(a) distribute by 1;
    
     kentyaohulk  ~/spark   SPARK-33806  tree /Users/kentyao/Downloads/spark/spark-3.1.0-SNAPSHOT-bin-20201202/spark-warehouse/test2/ -s
    /Users/kentyao/Downloads/spark/spark-3.1.0-SNAPSHOT-bin-20201202/spark-warehouse/test2/
    ├── [          0]  _SUCCESS
    ├── [        298]  part-00000-5dc19733-9405-414b-9681-d25c4d3e9ee6-c000.snappy.parquet
    └── [        426]  part-00001-5dc19733-9405-414b-9681-d25c4d3e9ee6-c000.snappy.parquet
    ```
    
    To avoid this, there are some options you can take.
    
    1. use `distribute by null`, let the data go to the partition 0
    2. set spark.sql.adaptive.enabled to true for Spark to automatically coalesce
    3. using hints instead of `distribute by`
    4. set spark.sql.shuffle.partitions to 1
    
    In this PR, we set the partition number to 1 in this particular case.
    
    1. avoid small file issues
    2. avoid unnecessary empty tasks when no adaptive execution
    
    no
    
    new test
    
    Closes #30800 from yaooqinn/SPARK-33806.
    
    Authored-by: Kent Yao <yaooqinn@hotmail.com>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>
    (cherry picked from commit 728a1298afa78c6acd7cdc4c21ee441120c34716)
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
@@ -27,7 +27,7 @@ import org.apache.spark.scheduler.{SparkListener, SparkListenerJobStart}
 import org.apache.spark.sql.catalyst.expressions.GenericRow
 import org.apache.spark.sql.catalyst.expressions.aggregate.{Complete, Partial}
 import org.apache.spark.sql.catalyst.optimizer.{ConstantFolding, ConvertToLocalRelation, NestedColumnAliasingSuite, ReorderAssociativeOperator}
-import org.apache.spark.sql.catalyst.plans.logical.Project
+import org.apache.spark.sql.catalyst.plans.logical.{Project, RepartitionByExpression}
 import org.apache.spark.sql.catalyst.util.StringUtils
 import org.apache.spark.sql.execution.adaptive.AdaptiveSparkPlanHelper
 import org.apache.spark.sql.execution.aggregate.{HashAggregateExec, ObjectHashAggregateExec, SortAggregateExec}
@@ -3780,6 +3780,19 @@ class SQLQuerySuite extends QueryTest with SharedSparkSession with AdaptiveSpark
       sql(s"CREATE TABLE $t (col1 INT, p1 STRING) USING PARQUET PARTITIONED BY (p1)")
       sql(s"INSERT INTO TABLE $t PARTITION (p1 = null) SELECT 0")
       checkAnswer(sql(s"SELECT * FROM $t"), Row(0, null))
+    }
+  }
+
+  test("limit partition num to 1 when distributing by foldable expressions") {
+    withSQLConf((SQLConf.SHUFFLE_PARTITIONS.key, "5")) {
+      Seq(1, "1, 2", null, "version()").foreach { expr =>
+        val plan = sql(s"select * from values (1), (2), (3) t(a) distribute by $expr")
+          .queryExecution.optimizedPlan
+        val res = plan.collect {
+          case r: RepartitionByExpression if r.numPartitions == 1 => true
+        }
+        assert(res.nonEmpty)
+      }
     }
   }
 }
