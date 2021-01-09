a842c04bc423cd1398891c407e7d82f99630963f
Anton Okolnychyi <aokolnychyi@apple.com>
2021-01-09 06:40:34 +0200

    [MINOR] Add stored procedure API (#850)
    ### What changes were proposed in this pull request?
    
    This PR adds a stored procedure API to 3.1.
    
    ### Why are the changes needed?
    
    This change is needed for our Iceberg integration in Spark 3.
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes but this change is isolated and won't affect anyone.
    
    ### How was this patch tested?
    
    This PR comes with a few tests. More tests in Iceberg.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
@@ -240,6 +240,7 @@ class Analyzer(override val catalogManager: CatalogManager)
       ResolveTableValuedFunctions ::
       ResolveNamespace(catalogManager) ::
       new ResolveCatalogs(catalogManager) ::
+      ResolveProcedures(catalogManager) ::
       ResolveUserSpecifiedColumns ::
       ResolveInsertInto ::
       ResolveRelations ::
