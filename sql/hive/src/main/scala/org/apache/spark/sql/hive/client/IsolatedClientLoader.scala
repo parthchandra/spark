52d5028982b76f50fb3bc07b63f9921751feb247
Chao Sun <sunchao@apple.com>
2021-01-15 14:06:50 -0800

    [SPARK-33212][BUILD] Upgrade to Hadoop 3.2.2 and move to shaded clients for Hadoop 3.x profile
    This:
    1. switches Spark to use shaded Hadoop clients, namely hadoop-client-api and hadoop-client-runtime, for Hadoop 3.x.
    2. upgrade built-in version for Hadoop 3.x to Hadoop 3.2.2
    
    Note that for Hadoop 2.7, we'll still use the same modules such as hadoop-client.
    
    In order to still keep default Hadoop profile to be hadoop-3.2, this defines the following Maven properties:
    
    ```
    hadoop-client-api.artifact
    hadoop-client-runtime.artifact
    hadoop-client-minicluster.artifact
    ```
    
    which default to:
    ```
    hadoop-client-api
    hadoop-client-runtime
    hadoop-client-minicluster
    ```
    but all switch to `hadoop-client` when the Hadoop profile is hadoop-2.7. A side affect from this is we'll import the same dependency multiple times. For this I have to disable Maven enforcer `banDuplicatePomDependencyVersions`.
    
    Besides above, there are the following changes:
    - explicitly add a few dependencies which are imported via transitive dependencies from Hadoop jars, but are removed from the shaded client jars.
    - removed the use of `ProxyUriUtils.getPath` from `ApplicationMaster` which is a server-side/private API.
    - modified `IsolatedClientLoader` to exclude `hadoop-auth` jars when Hadoop version is 3.x. This change should only matter when we're not sharing Hadoop classes with Spark (which is _mostly_ used in tests).
    
    Hadoop 3.2.2 is released with new features and bug fixes, so it's good for the Spark community to adopt it. However, latest Hadoop versions starting from Hadoop 3.2.1 have upgraded to use Guava 27+. In order to resolve Guava conflicts, this takes the approach by switching to shaded client jars provided by Hadoop. This also has the benefits of avoid pulling other 3rd party dependencies from Hadoop side so as to avoid more potential future conflicts.
    
    When people use Spark with `hadoop-provided` option, they should make sure class path contains `hadoop-client-api` and `hadoop-client-runtime` jars. In addition, they may need to make sure these jars appear before other Hadoop jars in the order. Otherwise, classes may be loaded from the other non-shaded Hadoop jars and cause potential conflicts.
    
    Relying on existing tests.
    
    Closes #30701 from sunchao/test-hadoop-3.2.2.
    
    Authored-by: Chao Sun <sunchao@apple.com>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/client/IsolatedClientLoader.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/client/IsolatedClientLoader.scala
@@ -113,17 +113,29 @@ private[hive] object IsolatedClientLoader extends Logging {
       hadoopVersion: String,
       ivyPath: Option[String],
       remoteRepos: String): Seq[URL] = {
+    val hadoopJarNames = if (hadoopVersion.startsWith("3")) {
+      Seq(s"org.apache.hadoop:hadoop-client-api:$hadoopVersion",
+        s"org.apache.hadoop:hadoop-client-runtime:$hadoopVersion")
+    } else {
+      Seq(s"org.apache.hadoop:hadoop-client:$hadoopVersion")
+    }
     val hiveArtifacts = if (version.fullVersion != "2.3.8.1-apple")  {
       version.extraDeps ++
         Seq("hive-metastore", "hive-exec", "hive-common", "hive-serde")
           .map(a => s"org.apache.hive:$a:${version.fullVersion}") ++
-        Seq("com.google.guava:guava:14.0.1",
-          s"org.apache.hadoop:hadoop-client:$hadoopVersion")
+        Seq("com.google.guava:guava:14.0.1") ++ hadoopJarNames
     } else {
       version.extraDeps ++
         Seq("hive-metastore", "hive-exec", "hive-common", "hive-serde")
-          .map(a => s"org.apache.hive:$a:${version.fullVersion}") ++
-        Seq(s"org.apache.hadoop:hadoop-client:$hadoopVersion")
+          .map(a => s"org.apache.hive:$a:${version.fullVersion}") ++ hadoopJarNames
+    }
+
+    val extraExclusions = if (hadoopVersion.startsWith("3")) {
+      // this introduced from lower version of Hive could conflict with jars in Hadoop 3.2+, so
+      // exclude here in favor of the ones in Hadoop 3.2+
+      Seq("org.apache.hadoop:hadoop-auth")
+    } else {
+      Seq.empty
     }
 
     val classpath = quietly {
@@ -132,7 +144,7 @@ private[hive] object IsolatedClientLoader extends Logging {
         SparkSubmitUtils.buildIvySettings(
           Some(remoteRepos),
           ivyPath),
-        exclusions = version.exclusions)
+        exclusions = version.exclusions ++ extraExclusions)
     }
     val allFiles = classpath.split(",").map(new File(_)).toSet
 
