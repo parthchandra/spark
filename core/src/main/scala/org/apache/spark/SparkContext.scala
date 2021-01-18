010fe182fa648e272d5f6723f293034dbaa508fd
Dongjoon Hyun <dongjoon@apache.org>
2021-01-17 16:54:01 -0800

    [SPARK-34142][CORE] Support Fallback Storage Cleanup during stopping SparkContext
    ### What changes were proposed in this pull request?
    
    This PR aims to support fallback storage clean-up during stopping `SparkContext`.
    
    ### Why are the changes needed?
    
    SPARK-33545 added `Support Fallback Storage during worker decommission` for the managed cloud-storages with TTL support.  Usually, it's one day. This PR will add an additional clean-up feature during stopping `SparkContext` in order to save some money before TTL or the other HDFS-compatible storage which doesn't have TTL support.
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, but this is a new feature.
    
    ### How was this patch tested?
    
    Pass the newly added UT.
    
    Closes #31215 from dongjoon-hyun/SPARK-34142.
    
    Authored-by: Dongjoon Hyun <dongjoon@apache.org>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>
    (cherry picked from commit 415506cc04f383c7006f46c8b9dd0ddf0f762d7d)
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -2081,6 +2081,7 @@ class SparkContext(config: SparkConf) extends Logging {
     Utils.tryLogNonFatalError {
       _plugins.foreach(_.shutdown())
     }
+    FallbackStorage.cleanUp(_conf, _hadoopConfiguration)
     Utils.tryLogNonFatalError {
       _eventLogger.foreach(_.stop())
     }
