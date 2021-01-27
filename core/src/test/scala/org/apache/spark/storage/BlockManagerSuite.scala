d99e4bba46391bfcaaf6e9e27b6c34e93dc3bc2e
Holden Karau <hkarau@apple.com>
2021-01-28 06:15:35 +0900

    [SPARK-34193][CORE] TorrentBroadcast block manager decommissioning race fix
    Allow broadcast blocks to be put during decommissioning since migrations don't apply to them and they may be stored as part of job exec.
    
    Potential race condition.
    
    Removal of race condition.
    
    New unit test.
    
    Closes #31298 from holdenk/SPARK-34193-torrentbroadcast-blockmanager-decommissioning-potential-race-condition.
    
    Authored-by: Holden Karau <hkarau@apple.com>
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>
    (cherry picked from commit 9d83d62f142ba89518194f176bb81adadc28951b)
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>

--- core/src/test/scala/org/apache/spark/storage/BlockManagerSuite.scala
+++ core/src/test/scala/org/apache/spark/storage/BlockManagerSuite.scala
@@ -1472,6 +1472,18 @@ class BlockManagerSuite extends SparkFunSuite with Matchers with BeforeAndAfterE
     assert(locs(blockIds(0)) == expectedLocs)
   }
 
+  test("SPARK-34193: Potential race condition during decommissioning with TorrentBroadcast") {
+    // Validate that we allow putting of broadcast blocks during decommissioning
+    val exec1 = "exec1"
+
+    val store = makeBlockManager(1000, exec1)
+    master.decommissionBlockManagers(Seq(exec1))
+    val a = new Array[Byte](1)
+    // Put a broadcast block, no exception
+    val broadcast0BlockId = BroadcastBlockId(0)
+    store.putSingle(broadcast0BlockId, a, StorageLevel.DISK_ONLY)
+  }
+
   class MockBlockTransferService(val maxFailures: Int) extends BlockTransferService {
     var numCalls = 0
     var tempFileManager: DownloadFileManager = null
