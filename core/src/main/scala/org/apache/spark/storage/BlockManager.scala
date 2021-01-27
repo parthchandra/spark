692b7f04774288bc1cd211c04feb854adf5bd86c
Holden Karau <hkarau@apple.com>
2021-01-28 06:15:35 +0900

    [SPARK-34193][CORE] TorrentBroadcast block manager decommissioning race fix
    Allow broadcast blocks to be put during decommissioning since migrations don't apply to them and they may be stored as part of job exec.
    
    Potential race condition.
    
    Removal of race condition.
    
    New unit test.
    
    Closes #31298 from holdenk/SPARK-34193-torrentbroadcast-blockmanager-decommissioning-potential-race-condition.
    
    Authored-by: Holden Karau <hkarau@apple.com>
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>
    (cherry picked from commit 9d83d62f142ba89518194f176bb81adadc28951b)
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>

--- core/src/main/scala/org/apache/spark/storage/BlockManager.scala
+++ core/src/main/scala/org/apache/spark/storage/BlockManager.scala
@@ -260,6 +260,14 @@ private[spark] class BlockManager(
     decommissioner.isDefined
   }
 
+  @inline final private def checkShouldStore(blockId: BlockId) = {
+    // Don't reject broadcast blocks since they may be stored during task exec and
+    // don't need to be migrated.
+    if (isDecommissioning() && !blockId.isBroadcast) {
+        throw new BlockSavedOnDecommissionedBlockManagerException(blockId)
+    }
+  }
+
   // This is a lazy val so someone can migrating RDDs even if they don't have a MigratableResolver
   // for shuffles. Used in BlockManagerDecommissioner & block puts.
   private[spark] lazy val migratableResolver: MigratableResolver = {
@@ -664,9 +672,7 @@ private[spark] class BlockManager(
       level: StorageLevel,
       classTag: ClassTag[_]): StreamCallbackWithID = {
 
-    if (isDecommissioning()) {
-       throw new BlockSavedOnDecommissionedBlockManagerException(blockId)
-    }
+    checkShouldStore(blockId)
 
     if (blockId.isShuffle) {
       logDebug(s"Putting shuffle block ${blockId}")
@@ -1315,9 +1321,7 @@ private[spark] class BlockManager(
 
     require(blockId != null, "BlockId is null")
     require(level != null && level.isValid, "StorageLevel is null or invalid")
-    if (isDecommissioning()) {
-      throw new BlockSavedOnDecommissionedBlockManagerException(blockId)
-    }
+    checkShouldStore(blockId)
 
     val putBlockInfo = {
       val newInfo = new BlockInfo(level, classTag, tellMaster)
