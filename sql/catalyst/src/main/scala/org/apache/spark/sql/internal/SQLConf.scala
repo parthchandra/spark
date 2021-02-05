6317c3cef5c98c7e96ef60825c38c645da356c6f
Wenchen Fan <cloud0fan@gmail.com>
2021-02-05 20:43:48 +0800

    [SPARK-34359][SQL][3.1] Add a legacy config to restore the output schema of SHOW DATABASES
    This backports https://github.com/apache/spark/pull/31474 to 3.1/3.0
    
    This is a followup of https://github.com/apache/spark/pull/26006
    
    In #26006 , we merged the v1 and v2 SHOW DATABASES/NAMESPACES commands, but we missed a behavior change that the output schema of SHOW DATABASES becomes different.
    
    This PR adds a legacy config to restore the old schema, with a migration guide item to mention this behavior change.
    
    Improve backward compatibility
    
    No (the legacy config is false by default)
    
    a new test
    
    Closes #31486 from cloud-fan/command-schema.
    
    Authored-by: Wenchen Fan <cloud0fan@gmail.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -3032,6 +3032,15 @@ object SQLConf {
     .stringConf
     .createOptional
 
+  val LEGACY_KEEP_COMMAND_OUTPUT_SCHEMA =
+    buildConf("spark.sql.legacy.keepCommandOutputSchema")
+      .internal()
+      .doc("When true, Spark will keep the output schema of commands such as SHOW DATABASES " +
+        "unchanged, for v1 catalog and/or table.")
+      .version("3.0.2")
+      .booleanConf
+      .createWithDefault(false)
+
   /**
    * Holds information about keys that have been deprecated.
    *
