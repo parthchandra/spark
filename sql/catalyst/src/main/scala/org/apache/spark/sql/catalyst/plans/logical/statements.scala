ba08e2fed1706a6ec99b513b856c438208b18ff6
Anton Okolnychyi <aokolnychyi@apple.com>
2021-02-25 19:28:37 -0800

    rdar://74251620 Add support for partition evolution (#909) (#947)

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/statements.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/statements.scala
@@ -502,3 +502,18 @@ case class AlterTableSetWriteDistributionAndOrderingStatement(
     tableName: Seq[String],
     distributionMode: String,
     ordering: Seq[SortOrder]) extends ParsedStatement
+
+/**
+ * ALTER TABLE ... ADD PARTITION FIELD ... statement, as parsed from SQL.
+ */
+case class AlterTableAddPartitionFieldStatement(
+    tableName: Seq[String],
+    transform: Transform,
+    name: Option[String]) extends ParsedStatement
+
+/**
+ * ALTER TABLE ... DROP PARTITION FIELD ... statement, as parsed from SQL.
+ */
+case class AlterTableDropPartitionFieldStatement(
+    tableName: Seq[String],
+    transform: Transform) extends ParsedStatement
