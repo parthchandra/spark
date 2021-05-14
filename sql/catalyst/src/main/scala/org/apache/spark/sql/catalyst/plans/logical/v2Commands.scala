03db0e1d280d64d34cdb16c8bf241f0c22080f75
Kyle Bendickson <kbendickson@apple.com>
2021-05-14 13:00:21 -0700

    [SPARK-34741][SQL] MergeIntoTable should avoid ambiguous reference in UpdateAction
    ### What changes were proposed in this pull request?
    
    This PR proposes to deduplicate the source table when there're conflicting attributes between the target table and the source table.
    
    ### Why are the changes needed?
    
    When resolving the `UpdateAction`, which could reference attributes from both target and source tables,  Spark should know clearly where the attribute comes from when there're conflicting attributes instead of picking up a random one.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Added a unit test and updated existing tests.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/v2Commands.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/v2Commands.scala
@@ -417,6 +417,7 @@ case class MergeIntoTable(
     matchedActions: Seq[MergeAction],
     notMatchedActions: Seq[MergeAction]) extends Command with SupportsSubquery {
   override def children: Seq[LogicalPlan] = Seq(targetTable, sourceTable)
+  def duplicateResolved: Boolean = targetTable.outputSet.intersect(sourceTable.outputSet).isEmpty
 }
 
 sealed abstract class MergeAction extends Expression with Unevaluable {
