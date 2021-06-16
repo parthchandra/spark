e5cd9712d870993c3f05776a34c9224321d55c8a
Chao Sun <sunchao@apple.com>
2021-06-16 13:28:07 -0700

    [SPARK-29250][BUILD] Upgrade to Hadoop 3.3.1
    This upgrade default Hadoop version from 3.2.1 to 3.3.1. The changes here are simply update the version number and dependency file.
    
    Hadoop 3.3.1 just came out, which comes with many client-side improvements such as for S3A/ABFS (20% faster when accessing S3). These are important for users who want to use Spark in a cloud environment.
    
    No
    
    - Existing unit tests in Spark
    - Manually tested using my S3 bucket for event log dir:
    ```
    bin/spark-shell \
      -c spark.hadoop.fs.s3a.access.key=$AWS_ACCESS_KEY_ID \
      -c spark.hadoop.fs.s3a.secret.key=$AWS_SECRET_ACCESS_KEY \
      -c spark.eventLog.enabled=true
      -c spark.eventLog.dir=s3a://<my-bucket>
    ```
    - Manually tested against docker-based YARN dev cluster, by running `SparkPi`.
    
    Closes #30135 from sunchao/SPARK-29250.
    
    Authored-by: Chao Sun <sunchao@apple.com>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/client/IsolatedClientLoader.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/client/IsolatedClientLoader.scala
@@ -111,7 +111,7 @@ private[hive] object IsolatedClientLoader extends Logging {
   def supportsHadoopShadedClient(hadoopVersion: String): Boolean = {
     VersionUtils.majorMinorPatchVersion(hadoopVersion).exists {
       case (3, 2, v) if v >= 2 => true
-      case (3, 3, _) if hadoopVersion.contains("apple") => true
+      case (3, 3, v) if v >= 1 => true
       case _ => false
     }
   }
